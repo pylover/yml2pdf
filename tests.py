01111c530be5fd6036d5bada1ca6bb95bff34407
Vahid <vahid.mardani@gmail.com>
2016-10-12 21:49:28 +0330

    Closes #1, and also closes #2.

--- tests.py
+++ tests.py
@@ -1,4 +1,6 @@
 
+from os.path import dirname, abspath, join, exists
+from os import mkdir
 import io
 import unittest
 
@@ -8,9 +10,12 @@ from yml2pdf import Template, Label
 class TemplateTestCase(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.temp_dir = abspath(join(dirname(__file__), 'temp'))
+        if not exists(self.temp_dir):
+            mkdir(self.temp_dir)
 
     def test_draw(self):
+        out_filename = join(self.temp_dir, 'test_draw.pdf')
 
         yml = """
 
@@ -19,8 +24,18 @@ class TemplateTestCase(unittest.TestCase):
 
         body:
           - !label
+            text: 'Page: %(page)s'
+            pos: [20, 20]
+
+
+          - !label
             text: 'Lorem Ipsum'
-            pos: [10, 20]
+            pos: [20, 100]
+
+          - !label
+            text: 'لورم ایپسام'
+            pos: [20, 200]
+
 
         """
 
@@ -30,7 +45,14 @@ class TemplateTestCase(unittest.TestCase):
         self.assertEqual(t.data['width'], 800)
         self.assertEqual(t.data['height'], 600)
         self.assertIsInstance(t.data['body'][0], Label)
-        self.assertEqual(t.data['body'][0].text, 'Lorem Ipsum')
+        self.assertEqual(t.data['body'][1].text, 'Lorem Ipsum')
 
-        t.draw()
-        self.assertEqual(len(out.getbuffer()) // 100, 15)
+        for i in range(5):
+            t.draw_new_page(dict(page=i+1))
+        t.save()
+        out.seek(0)
+
+        with open(out_filename, 'wb') as f:
+            f.write(out.read())
+        self.assertEqual(len(out.getbuffer()) // 100, 37)
+
--- yml2pdf.py
+++ yml2pdf.py
@@ -7,6 +7,9 @@ from reportlab.pdfgen import canvas
 from reportlab.pdfbase.pdfmetrics import stringWidth
 
 
+__version__ = '0.1.0-dev0'
+
+
 # noinspection PyClassicStyleClass, PyAbstractClass
 class Template(canvas.Canvas):
     """
@@ -21,6 +24,7 @@ class Template(canvas.Canvas):
         # yaml.add_constructor('!label', Label)
         self.data = yaml.load(yml)
         self.outfile = out_file or io.BytesIO()
+        self.current_page = 0
         canvas.Canvas.__init__(self, self.outfile, pagesize=(self.width, self.height))
 
     def measure_width(self, text):
@@ -34,28 +38,18 @@ class Template(canvas.Canvas):
     def height(self):
         return self.data['height']
 
-    def draw(self):
+    def draw_new_page(self, context):
+        if self.current_page > 0:
+            self.showPage()
+        self.current_page += 1
+
         for element in self.data['body']:
-            element.draw(self)
-        self.save()
-
-# def draw_center(self, y, text):
-#     self.drawCentredString(self.width / 2.0, y, text)
-#
-
-
-# from reportlab.graphics.barcode import code128
-# def draw_barcode(self, code, x, y, width, height):
-#     barcode = code128.Code128(
-#         code,
-#         quiet=0,
-#         barWidth=width,
-#         barHeight=height)
-#     barcode.drawOn(self, x, y)
+            element.draw(self, context)
 
 
 class Element(yaml.YAMLObject):
-    def draw(self, canvas: Template):
+
+    def draw(self, canvas: Template, context: dict):
         raise NotImplementedError()
 
 
@@ -79,5 +73,20 @@ class Label(Element):
 
     # __slots__ = ('text', 'pos')
 
-    def draw(self, tableau: Template):
-        tableau.drawString(self.pos[0], self.pos[1], self.text, mode=self.mode)
+    def draw(self, tableau, context):
+        tableau.drawString(self.pos[0], self.pos[1], self.text % context, mode=self.mode)
+
+
+# def draw_center(self, y, text):
+#     self.drawCentredString(self.width / 2.0, y, text)
+#
+
+
+# from reportlab.graphics.barcode import code128
+# def draw_barcode(self, code, x, y, width, height):
+#     barcode = code128.Code128(
+#         code,
+#         quiet=0,
+#         barWidth=width,
+#         barHeight=height)
+#     barcode.drawOn(self, x, y)
