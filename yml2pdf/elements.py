a8d4ebf0538c94fc387873a7f8d5bcc81263a490
pylover <vahid@carrene.com>
2017-05-23 15:24:12 +0430

    building flowable style acceptance structure

--- yml2pdf/elements.py
+++ yml2pdf/elements.py
@@ -2,7 +2,6 @@
 import yaml
 
 from reportlab import platypus
-from reportlab.lib.styles import getSampleStyleSheet
 from reportlab.lib import styles
 
 from yml2pdf.helpers import to_camel_case
@@ -15,33 +14,44 @@ class Element(yaml.YAMLObject):
 class Flowable(Element):
     __flowable_class__ = None
     __flowable_attributes__ = None
+    __flowable_style_class__ = None
+
+    styles = {}
+
+    def create_style(self):
+        result = self.__class__.styles.copy()
+        if self.styles is not self.__class__.styles:
+            result.update(self.styles)
+        return self.__flowable_style_class__(**result)
+
+    def __getattr__(self, item):
+        if item == 'styles':
+            return self.create_style()
+        return object.__getattribute__(self, item)
 
     def to_flowable(self):
-        flowable_attributes = {}
-        for k, v in self.__flowable_attributes__.items():
-            if k in self.__dict__:
-                flowable_attributes[to_camel_case(k)] = self.__dict__[k]
-            else:
-                flowable_attributes[to_camel_case(k)] = v
-
-        return self.__flowable_class__(**flowable_attributes)
-
-
-class ParagraphStyle(Flowable):
-    yaml_tag = '!ParagraphStyle'
-    yaml_flow_style = False
-    __flowable_class__ = styles.ParagraphStyle
-    # __flowable_attributes__ = {'style': getSampleStyleSheet()['Normal']}
-    __flowable_attributes__ = {'font_size': 10, 'leading': 12}
+        return self.__flowable_class__(**{
+            to_camel_case(k): getattr(self, k, v) for k, v in self.__flowable_attributes__.items()
+        })
 
 
 class Paragraph(Flowable):
     yaml_tag = '!Paragraph'
     __flowable_class__ = platypus.Paragraph
-    __flowable_attributes__ = {'text': '', 'style': getSampleStyleSheet()['Normal']}
+    __flowable_attributes__ = {
+        'text': '',
+        'styles': styles.getSampleStyleSheet()['Normal']
+    }
+    __flowable_style_class__ = styles.ParagraphStyle
 
+    styles = dict(
+
+    )
 
 class Spacer(Flowable):
     yaml_tag = '!Spacer'
     __flowable_class__ = platypus.Spacer
-    __flowable_attributes__ = {'width': 0, 'height': 0}
+    __flowable_attributes__ = {
+        'width': 0,
+        'height': 0
+    }
